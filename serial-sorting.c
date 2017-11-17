5b77748097fe0bbdf2567624fa478fdeaf250d7c
MSPigl <31660798+MSPigl@users.noreply.github.com>
2017-11-17 09:59:39 -0500

    Update serial-sorting.c

--- serial-sorting.c
+++ serial-sorting.c
@@ -15,7 +15,7 @@
        {
           if (array[j] > array[j + 1])
           {
-             int temp = array[j]
+             int temp = array[j];
              array[j] = array[j + 1];
              array[j + 1] = temp;
              
