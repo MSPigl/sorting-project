4ef67930068f65b7ee9f50584b6aa3f64238128c
MSPigl <31660798+MSPigl@users.noreply.github.com>
2017-11-17 08:33:34 -0500

    Testing

--- array-generator.c
+++ array-generator.c
@@ -5,3 +5,10 @@ void* makeArray(int numElement, int elementType)
 {
    return malloc(numElement * elementType);
 }
+
+int main()
+{
+   int* intArray = (int *)makeArray(10, sizeof(int));
+   float* floatArray = (float *)makeArray(10, sizeof(float));
+   char* charArray = (char *)makeArray(10, sizeof(char));
+}
