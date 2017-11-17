f7e80e7b53bc9f65f24a63b72b3b7ff9dac054f8
MSPigl <31660798+MSPigl@users.noreply.github.com>
2017-11-17 08:44:54 -0500

    Update array-generator.c

--- array-generator.c
+++ array-generator.c
@@ -1,16 +1,40 @@
 #include <stdlib.h>
+#include <time.h>
+
+/*
+ * Function to fill an array with pseudo-random integers
+ */
+int* fillArray(int size, int* array)
+{
+   srand(time(NULL));
+   int r, i; 
+   for (i = 0; i < size; i++)
+   {
+      r = rand();
+      array[i] = r;
+   }
+}
 
 /*
  * Function to generate input arrays
  */
-void* makeArray(int numElement, int elementType)
+int* makeArray(int size)
 {
-   return malloc(numElement * elementType);
+   int* array = (int *)malloc(numElement * sizeof(int));
+   fillArray(size, array);
+   
+   return array;
 }
 
 int main()
 {
-   int* intArray = (int *)makeArray(10, sizeof(int));
-   float* floatArray = (float *)makeArray(10, sizeof(float));
-   char* charArray = (char *)makeArray(10, sizeof(char));
+   int* intArray = makeArray(10, sizeof(int));
+   
+   int i;
+   printf("{ ");
+   for (i = 0; i < 10; i++)
+   {
+      printf("%d ", intArray[i]);
+   }
+   printf("}\n");
 }
