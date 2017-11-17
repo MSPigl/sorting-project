095547e56d750326a181783bbeee8b80d9f8d9e4
MSPigl <31660798+MSPigl@users.noreply.github.com>
2017-11-17 10:53:15 -0500

    Debugging

--- serial-sorting.c
+++ serial-sorting.c
@@ -27,6 +27,55 @@
     }
  }
 
+void merge_up(int *arr, int n)
+{
+   int step=n/2,i,j,k,temp;
+   while (step > 0)
+   {
+      for (i=0; i < n; i+=step*2) 
+      {
+         for (j=i,k=0;k < step;j++,k++)
+         {
+            if (arr[j] > arr[j+step])
+            {
+              // swap
+              temp = arr[j];
+              arr[j]=arr[j+step];
+              arr[j+step]=temp;
+            }
+         }
+      }
+      step /= 2;
+   }
+}
+
+void merge_down(int *arr, int n)
+{
+   int step=n/2,i,j,k,temp;
+   while (step > 0)
+   {
+      for (i=0; i < n; i+=step*2)
+      {
+         for (j=i,k=0;k < step;j++,k++) 
+         {
+            if (arr[j] < arr[j+step])
+            {
+               // swap
+               temp = arr[j];
+               arr[j]=arr[j+step];
+               arr[j+step]=temp;
+            }
+         }
+      }
+      step /= 2;
+   }
+}
+
+void bitonicSort()
+{
+   
+}
+
 int main()
 {
    int array[] = {5, 7, 1, 6, 2, 20, 35, 3, 9, 10}; 
