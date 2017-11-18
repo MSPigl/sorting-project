5a3a2fda934f0dd979c212b6aa190e863e14b107
MSPigl <31660798+MSPigl@users.noreply.github.com>
2017-11-18 14:08:23 -0500

    Testing quicksort

--- serial-sorting.c
+++ serial-sorting.c
@@ -84,13 +84,56 @@ void bitonicSort(int size, int* array)
       }
    }
 }
+ 
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+ 
+    for (int j = low; j <= high- 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int index = partition(arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, index - 1);
+        quickSort(arr, index + 1, high);
+    }
+}
 
 int main()
 {
    int array[] = {5, 7, 1, 6, 2, 20, 35, 3, 9, 10}; 
    
    //bubbleSort(10, array);
-   bitonicSort(10, array);
+   //bitonicSort(10, array);
+   quicksort(array, 0 9);
    int i;
    for (i = 0; i < 10; i++)
    {
