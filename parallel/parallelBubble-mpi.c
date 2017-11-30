8af929773815ce6b499a0a9fcd166bd017fb586a
MSPigl <31660798+MSPigl@users.noreply.github.com>
2017-11-29 20:19:51 -0500

    Update parallelBubble-mpi.c

--- parallel/parallelBubble-mpi.c
+++ parallel/parallelBubble-mpi.c
@@ -41,6 +41,7 @@ int main(int argc, char** argv)
 				//set up array
 				data = makeArray(size);
 				printf("Size is %d\n", size);
+			 	printf("%d | element at 0: %d\n", myRank, myArray[0]);
 		}
 		
 		MPI_Barrier(MPI_COMM_WORLD);
