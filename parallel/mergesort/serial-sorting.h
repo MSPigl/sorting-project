6e278e89dca032d79579403f8d7f68fad10d4af7
MSPigl <ms11pigl@siena.edu>
2017-11-30 18:22:01 -0500

    Organizing

---
+++ parallel/mergesort/serial-sorting.h
@@ -0,0 +1,21 @@
+/*
+ * Header for serial-sorting-c
+ */
+ 
+void bubbleSort(int arr[], int size);
+
+void merge_up(int arr[], int n);
+
+void merge_down(int arr[], int n);
+
+void bitonicSort(int arr[], int size);
+
+void swap(int* a, int* b);
+
+int partition (int arr[], int low, int high);
+
+void quickSort(int arr[], int low, int high);
+
+void merge(int arr[], int left, int mid, int right);
+
+void mergeSort(int arr[], int left, int right);
