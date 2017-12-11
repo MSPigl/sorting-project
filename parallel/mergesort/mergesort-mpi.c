c888a54a4e01c5514cfeff120d556b83e54bcc7a
MSPigl <31660798+MSPigl@users.noreply.github.com>
2017-12-10 19:34:48 -0500

    Update mergesort-mpi.c

--- parallel/mergesort/mergesort-mpi.c
+++ parallel/mergesort/mergesort-mpi.c
@@ -3,150 +3,60 @@
 #include <mpi.h>
 
 #include "array-util.h"
+#include "serial-sorting.h"
 
-void merge(int *, int *, int, int, int);
-void mergeSort(int *, int *, int, int);
-
-int main(int argc, char** argv) {
-	int c, n;
-	
-	/********** Initialize MPI **********/
-	int world_rank;
-	int world_size;
-  int* original_array;
-  double t1, t2;
+int main(int argc, char** argv)
+{
+  int myRank, numProcs;
+  int *data, *myArray, size, chunk;
+  double start, end;
   
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Init(&argc, &argv);
   
-  n = atoi(argv[1]);
   
-  if (world_rank == 0)
+  if (argc != 2)
   {
-    original_array = makeArray(n);
-    t1 = MPI_Wtime();
+    fprintf(stderr, "Usage: <array size>\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
   }
-		
-	/********** Divide the array in equal-sized chunks **********/
-	int size = n/world_size;
-	
-	/********** Send each subarray to each process **********/
-	int *sub_array = malloc(size * sizeof(int));
-	MPI_Scatter(original_array, size, MPI_INT, sub_array, size, MPI_INT, 0, MPI_COMM_WORLD);
-	
-	/********** Perform the mergesort on each process **********/
-	int *tmp_array = malloc(size * sizeof(int));
-	mergeSort(sub_array, tmp_array, 0, (size - 1));
-	
-	/********** Gather the sorted subarrays into one **********/
-	int *sorted = NULL;
-	if(world_rank == 0) {
-		
-		sorted = malloc(n * sizeof(int));
-		
-		}
-	
-	MPI_Gather(sub_array, size, MPI_INT, sorted, size, MPI_INT, 0, MPI_COMM_WORLD);
-	
-	/********** Make the final mergeSort call **********/
-	if(world_rank == 0) {
-		
-		int *other_array = malloc(n * sizeof(int));
-		mergeSort(sorted, other_array, 0, (n - 1));
-		
-		t2 = MPI_Wtime();
-    
-    printf( "Elapsed time is %f\n", t2 - t1 ); 
-    
-		/********** Clean up root **********/
-		free(sorted);
-		free(other_array);
-			
-		}
-	
-	/********** Clean up rest **********/
-	free(original_array);
-	free(sub_array);
-	free(tmp_array);
-	
-	/********** Finalize MPI **********/
-	MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Finalize();
-	
-	}
+  
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);	
 
-/********** Merge Function **********/
-void merge(int *a, int *b, int l, int m, int r) {
-	
-	int h, i, j, k;
-	h = l;
-	i = l;
-	j = m + 1;
-	
-	while((h <= m) && (j <= r)) {
-		
-		if(a[h] <= a[j]) {
-			
-			b[i] = a[h];
-			h++;
-			
-			}
-			
-		else {
-			
-			b[i] = a[j];
-			j++;
-			
-			}
-			
-		i++;
-		
-		}
-		
-	if(m < h) {
-		
-		for(k = j; k <= r; k++) {
-			
-			b[i] = a[k];
-			i++;
-			
-			}
-			
-		}
-		
-	else {
-		
-		for(k = h; k <= m; k++) {
-			
-			b[i] = a[k];
-			i++;
-			
-			}
-			
-		}
-		
-	for(k = l; k <= r; k++) {
-		
-		a[k] = b[k];
-		
-		}
-		
-	}
-
-/********** Recursive Merge Function **********/
-void mergeSort(int *a, int *b, int l, int r) {
-	
-	int m;
-	
-	if(l < r) {
-		
-		m = (l + r)/2;
-		
-		mergeSort(a, b, l, m);
-		mergeSort(a, b, (m + 1), r);
-		merge(a, b, l, m, r);
-		
-		}
-		
-	}
+  size = atoi(argv[1]);
+  chunk = size / numProcs;
+  
+  if (myRank == 0)
+  {
+    data = makeArray(size);
+    start = MPI_Wtime();
+  }
+  
+  myArray = (int *)malloc(chunk * sizeof(int));
+  
+  MPI_Scatter(data, chunk, MPI_INT, myArray, chunk, MPI_INT, 0, MPI_COMM_WORLD);
+  
+  mergeSort(myArray, 0, chunk - 1);
+  
+  MPI_Gather(myArray, chunk, MPI_INT, data, chunk, MPI_INT, 0, MPI_COMM_WORLD);
+  
+  if (myRank == 0)
+  {
+    mergeSort(data, 0, size - 1);
+    
+    end = MPI_Wtime();
+#ifdef DEBUG
+{
+ printArray(data, size); 
+}
+#endif
+    printf( "Elapsed time is %f\n", end - start ); 
+    
+    free(data);
+  }
+  
+  free(myArray);
+  
+  MPI_Finalize();
+  return 0;
+}
